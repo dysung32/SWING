42950823314b05dd3ca2aab52a22b567cc36c5a9
LEEYEONSOO <noprob1325@naver.com>
2023-03-13 16:43:33 +0900

    back-end add Dockerfile

---
+++ back-springboot/src/main/java/com/swing/user/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.swing.user.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = {"*"}, maxAge = 6000)
+@RestController
+public class UserController {
+	
+}
--- back-springboot/src/test/java/com/swing/SwingApplicationTests.java
+++ back-springboot/src/test/java/com/swing/SwingApplicationTests.java
@@ -1,10 +1,10 @@
-package swing;
+package com.swing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SwingApplicationTests {
 
 	@Test
 	void contextLoads() {

83dc8a1919c78e30a5238aac12be3ba701b900a4
isks2 <isks2@naver.com>
2023-03-16 17:45:14 +0900

    [FE] User Login (kakao login으로 이동) 구현

--- front/src/pages/LogIn.js
+++ front/src/pages/LogIn.js
@@ -18,6 +18,44 @@ function LogIn() {
     navigate("/");
   };
 
+  // const loginWithKakao = () => {
+  //   console.log("함수 실행됨");
+  //   window.Kakao.Auth.login({
+  //     scope: "profile_nickname, profile_image",
+  //     success: getProfile,
+  //   });
+  //   console.log("함수2");
+  // };
+
+  // const getProfile = () => {
+  //   console.log("함수 실행됨2");
+  //   window.Kakao.API.request({
+  //     url: "http://localhost:3000/login",
+  //     success: (res) => {
+  //       console.log("성공");
+  //       const kakao_account = res.kakao_account;
+  //       let emails = kakao_account.email;
+  //       if (emails == null) {
+  //         if (this.$route.path !== "/") this.$router.push({ name: "home" });
+  //       } else {
+  //         const req_body = {
+  //           userId: "kakao" + res.id,
+  //           userName: kakao_account.profile.nickname,
+  //           email: emails,
+  //           type: "kakao",
+  //         };
+  //         this.socialLogin(req_body);
+  //       }
+  //     },
+  //   });
+  // };
+
+  const kakaoLogin = () => {
+    window.Kakao.Auth.authorize({
+      redirectUri: "http://localhost:3000/login",
+    });
+  };
+
   return (
     <>
       <LogInWrapper>
@@ -30,7 +68,7 @@ function LogIn() {
             <LogInBtn border="#B4B4B4" image={Google}>
               Google로 시작하기
             </LogInBtn>
-            <LogInBtn color="#F7E600" image={Kakao}>
+            <LogInBtn color="#F7E600" image={Kakao} onClick={kakaoLogin}>
               카카오로 시작하기
             </LogInBtn>
           </LogInBtnContainer>

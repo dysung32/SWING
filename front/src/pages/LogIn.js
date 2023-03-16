00254d692e162673cbe594d91d6adff2a529f63f
isks2 <isks2@naver.com>
2023-03-16 13:26:35 +0900

    [FE] LogIn 정적 페이지 구현

--- front/src/pages/LogIn.js
+++ front/src/pages/LogIn.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { LogInWrapper, LogInContainer } from "../styles/LogInEmotion";
+import { useNavigate } from "react-router-dom";
+import {
+  LogInWrapper,
+  LogInContainer,
+  ExpImg,
+  LogoImg,
+  LogInBtnContainer,
+  LogInBtn,
+} from "../styles/LogInEmotion";
+import RoundLogo from "../styles/CommonEmotion";
+import Google from "../assets/google_icon.png";
+import Kakao from "../assets/kakaotalk_icon.png";
+
 function LogIn() {
+  const navigate = useNavigate();
+  const onClickLogo = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <LogInWrapper>
-        <LogInContainer></LogInContainer>
+        <LogInContainer>
+          <ExpImg />
+          <LogoImg>
+            <RoundLogo alt="logo" onClick={onClickLogo} size="70%" />
+          </LogoImg>
+          <LogInBtnContainer>
+            <LogInBtn border="#B4B4B4" image={Google}>
+              Google로 시작하기
+            </LogInBtn>
+            <LogInBtn color="#F7E600" image={Kakao}>
+              카카오로 시작하기
+            </LogInBtn>
+          </LogInBtnContainer>
+        </LogInContainer>
       </LogInWrapper>
     </>
   );
--- front/src/styles/LogInEmotion.js
+++ front/src/styles/LogInEmotion.js
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
+import logInExpImg from "../assets/login_expImg.png";
 
 const LogInWrapper = styled.div`
+  display: flex;
+  align-items: center;
   width: 100vw;
   height: 100vh;
   padding: 0 10%;
@@ -9,8 +12,64 @@ const LogInWrapper = styled.div`
 `;
 
 const LogInContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(7, 1fr);
+  grid-template-columns: repeat(12, 1fr);
+  column-gap: 2rem;
+  grid-template-areas:
+    ". . . . . . . . . . . ."
+    ". expImg expImg expImg expImg . logoImg logoImg logoImg logoImg logoImg ."
+    ". expImg expImg expImg expImg . logoImg logoImg logoImg logoImg logoImg ."
+    ". expImg expImg expImg expImg . logoImg logoImg logoImg logoImg logoImg ."
+    ". expImg expImg expImg expImg . logInBtnCon logInBtnCon logInBtnCon logInBtnCon logInBtnCon ."
+    ". expImg expImg expImg expImg . logInBtnCon logInBtnCon logInBtnCon logInBtnCon logInBtnCon ."
+    ". . . . . . . . . . . .";
   width: 100%;
-  height: 80%;
+  height: 68vh;
+  box-sizing: border-box;
+  border-radius: 0.75rem;
   background-color: #ffffff;
 `;
-export { LogInWrapper, LogInContainer };
+
+const ExpImg = styled.div`
+  grid-area: expImg;
+  background: center / contain no-repeat url(${logInExpImg});
+`;
+const LogoImg = styled.div`
+  display: flex;
+  justify-content: center;
+  grid-area: logoImg;
+  box-sizing: border-box;
+`;
+
+const LogInBtnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  grid-area: logInBtnCon;
+`;
+
+const LogInBtn = styled.div`
+  width: 100%;
+  padding: 1.3rem 0;
+  border: ${(props) => (props.border ? `1px solid ${props.border}` : "none")};
+  border-radius: 0.5rem;
+  box-sizing: border-box;
+  background-color: ${(props) => (props.color ? props.color : "#ffffff")};
+  background-image: url(${(props) => (props.image ? props.image : "")});
+  background-size: 3rem;
+  background-repeat: no-repeat;
+  background-position-x: 1rem;
+  background-position-y: center;
+  text-align: center;
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+export {
+  LogInWrapper,
+  LogInContainer,
+  ExpImg,
+  LogoImg,
+  LogInBtnContainer,
+  LogInBtn,
+};

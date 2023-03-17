76dbcf4201eaa7d302251701fbf406a9c1b1869c
YU DAVID <loveneverfails95@naver.com>
2023-03-17 09:38:12 +0900

    [BE] django lightening 완료
    Former-commit-id: fd1cc423343f35903ae246c81cf94910921994d1

--- back-django/swing/settings.py
+++ back-django/swing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$=9@qb1pi!c7x6@k0$np%g)_9#_*ub(e25@zp3&$k0e9_&_4sw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://j8a405.p.ssafy.io']
+ALLOWED_HOSTS = []
 
 # Application definition
 

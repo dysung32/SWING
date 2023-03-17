cf7865595d8ac755b65febdce859570a6f4ceda3
isks2 <isks2@naver.com>
2023-03-17 13:36:49 +0900

    [FE] merge 이슈로 commit

--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import RoundLogo from '../styles/CommonEmotion';
+import { RoundLogo } from '../styles/CommonEmotion';
 import { Nav, NavItems } from '../styles/NavEmotion';
 
 function NavBar() {

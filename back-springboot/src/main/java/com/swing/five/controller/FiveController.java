ea310c45d0d599fa43e6a0cb800e402a3ac5f178
LEEYEONSOO <noprob1325@naver.com>
2023-03-20 11:30:47 +0900

    [BE] Spring Boot - Sentency, Hi-five, Speedoodle, Note 기본 frame 수정

--- back-springboot/src/main/java/com/swing/five/controller/FiveController.java
+++ back-springboot/src/main/java/com/swing/five/controller/FiveController.java
@@ -1,36 +1,19 @@
 package com.swing.five.controller;
 
-import com.swing.user.model.service.UserService;
+import com.swing.five.model.service.FiveService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @CrossOrigin(origins = {"*"}, maxAge = 6000)
 @RestController
-@RequestMapping("/user")
-@Api(tags = {"회원 관리 API"})
+@RequestMapping("/five")
+@Api(tags = {"Hi-five 관리 API"})
 public class FiveController {
 	
 	@Autowired
-	private UserService userService;
-	
-	@ApiOperation(value = "Swagger Test", notes = "뭐요!", response = Map.class)
-	@GetMapping("")
-	public ResponseEntity<?> test() {
-		Map<String, Object> resultMap = new HashMap<>();
-		HttpStatus status = HttpStatus.OK;
-		resultMap.put("message", userService.test());
-		
-		return new ResponseEntity<>(resultMap, status);
-	}
+	private FiveService fiveService;
 	
 }

eb6d21d2701c289f226ee71eb6abaf0e3e2a7e2e
isks2 <isks2@naver.com>
2023-03-20 16:44:32 +0900

    [FE] 마이페이지 퍼블리싱

--- front/src/pages/MyPage.js
+++ front/src/pages/MyPage.js
@@ -1,10 +1,103 @@
 import React from 'react';
-import { MyPageWrapper } from '../styles/MyPageEmotion';
+import {
+  MyPageWrapper,
+  MyPageContentContainer,
+  MyPageMainConatiner,
+  MyPageProfileConatiner,
+  MyPageIntroConatiner,
+  MyPageHistoryConatiner,
+  MyPageHistoryHeader,
+  MyPageHistoryList,
+} from '../styles/MyPageEmotion';
+import { GameTitle, CommonBtn } from '../styles/CommonEmotion';
+import { H1, H3, H5, P1 } from '../styles/Fonts';
+import { colors } from '../styles/ColorPalette';
 
 function MyPage() {
+  const nickName = 'Sanghwa';
+
+  const historyList = [
+    {
+      date: '2023.03.01',
+      title: '나랑 같이 놀 사람',
+      rank: 2,
+    },
+    {
+      date: '2023.03.02',
+      title: '공부합시다',
+      rank: 3,
+    },
+    {
+      date: '2023.03.03',
+      title: '놀자아',
+      rank: 1,
+    },
+    {
+      date: '2023.03.05',
+      title: 'A405',
+      rank: 2,
+    },
+    {
+      date: '2023.03.07',
+      title: 'SSAFY 8기',
+      rank: 1,
+    },
+    {
+      date: '2023.03.07',
+      title: 'SSAFY 8기',
+      rank: 1,
+    },
+    {
+      date: '2023.03.07',
+      title: 'SSAFY 8기',
+      rank: 1,
+    },
+  ];
+
+  const renderList = historyList.map((history) => {
+    return (
+      <MyPageHistoryList>
+        <P1 color={colors.gameBlue500}>{history.date}</P1>
+        <P1 color={colors.gameBlue500}>{history.title}</P1>
+        <P1 color={colors.gameBlue500}>{history.rank}등</P1>
+      </MyPageHistoryList>
+    );
+  });
   return (
     <>
-      <MyPageWrapper>MyPage</MyPageWrapper>
+      <MyPageWrapper>
+        <GameTitle>
+          <H1
+            color={colors.white}
+            outline={colors.gameBlue500}
+            outlineWeight={2}
+            align='center'
+          >
+            마이페이지
+          </H1>
+        </GameTitle>
+        <MyPageContentContainer>
+          <MyPageMainConatiner>
+            <MyPageIntroConatiner>
+              <H3>Hi, {nickName}!</H3>
+              <P1>
+                SWING을 즐기고 계신가요? <br /> 마이페이지에서는 예전
+                SpeeDoodle의 기록을 보기 위한 <br /> 히스토리와 회원님의 정보를
+                수정하기 위한 프로필이 있습니다.
+              </P1>
+            </MyPageIntroConatiner>
+            <MyPageHistoryConatiner>
+              <MyPageHistoryHeader>
+                <H5 color={colors.gameBlue500}>날짜</H5>
+                <H5 color={colors.gameBlue500}>방제목</H5>
+                <H5 color={colors.gameBlue500}>등수</H5>
+              </MyPageHistoryHeader>
+              {renderList}
+            </MyPageHistoryConatiner>
+          </MyPageMainConatiner>
+          <MyPageProfileConatiner></MyPageProfileConatiner>
+        </MyPageContentContainer>
+      </MyPageWrapper>
     </>
   );
 }
--- front/src/styles/MyPageEmotion.js
+++ front/src/styles/MyPageEmotion.js
@@ -1,13 +1,83 @@
 import styled from '@emotion/styled';
 import { colors } from './ColorPalette';
+import MyPageSwing from '../assets/mypage_swing.png';
 
 export const MyPageWrapper = styled.div`
   display: flex;
-  // align-items: center;
+  align-items: center;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
   padding: 0 10%;
   padding-top: 9rem;
   box-sizing: border-box;
   background-color: ${colors.studyBlue300};
+`;
+
+export const MyPageContentContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(1, 1fr);
+  grid-template-columns: repeat(12, 1fr);
+  column-gap: 2rem;
+  width: 100%;
+  height: 75%;
+  box-sizing: border-box;
+`;
+
+export const MyPageMainConatiner = styled.div`
+  grid-row-start: 1;
+  grid-row-end: 1;
+  grid-column-start: 1;
+  grid-column-end: 10;
+  box-sizing: border-box;
+`;
+
+export const MyPageProfileConatiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-row-start: 1;
+  grid-row-end: 1;
+  grid-column-start: 10;
+  grid-column-end: 13;
+  background-color: blue;
+  border-radius: 1rem;
+  box-sizing: border-box;
+`;
+
+export const MyPageIntroConatiner = styled.div`
+  widht: 100%;
+  height: 30%;
+  padding: 1.5rem;
+  background-color: ${colors.white};
+  background-image: url(${MyPageSwing});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: right 10% bottom;
+  border-radius: 1rem;
+  box-sizing: border-box;
+`;
+
+export const MyPageHistoryConatiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  widht: 100%;
+  height: calc(70% - 2rem);
+  padding: 1.5rem;
+  margin-top: 2rem;
+  background-color: ${colors.white};
+  border-radius: 1rem;
+  box-sizing: border-box;
+`;
+
+export const MyPageHistoryHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+`;
+export const MyPageHistoryList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 0.5rem;
+  border-bottom: 2px solid ${colors.gameBlue500};
+  box-sizing: border-box;
 `;

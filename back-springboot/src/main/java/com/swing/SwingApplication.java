bb25e4abcb2afc3d74b5fd179058cb5aa3cc16fd
LEEYEONSOO <noprob1325@naver.com>
2023-03-21 12:59:00 +0900

    [BE] Spring Boot - S3 파일 서버를 이용한 파일 업로드 구현 Dependency 추가, application.yml 수정 등

--- back-springboot/src/main/java/com/swing/SwingApplication.java
+++ back-springboot/src/main/java/com/swing/SwingApplication.java
@@ -3,8 +3,13 @@ package com.swing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SwingApplication {
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+) public class SwingApplication {
 
 	public static void main(String[] args) {
 		System.setProperty("server.servlet.context-path", "/api");

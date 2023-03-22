69c7e91cc278994f365a376a95541a791550f156
isks2 <isks2@naver.com>
2023-03-22 17:59:25 +0900

    [FE] 홈화면 스크롤 시 다음 화면으로 위치 이동 구현

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,6 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useMemo, useState } from 'react';
 import GoogleLogin from '../auth/GoogleLogin';
-import { HomeWrapper } from '../styles/HomeEmotion';
+import {
+  HomeWrapper,
+  HomeHeroContainer,
+  HomeSentencyContainer,
+  HomeHiFiveContainer,
+  HomeSpeedoodleContainer,
+  Divider,
+} from '../styles/HomeEmotion';
 
 function Home() {
   // useEffect(() => {
@@ -9,9 +16,100 @@ function Home() {
   //     GoogleLogin(code);
   //   }
   // });
+  const [scrollIndex, setScrollIndex] = useState(1);
+  const DIVIDER_HEIGHT = 5;
+  const outerDivRef = useRef();
+  const scrollHeroRef = useRef(null);
+  const scrolSentencylRef = useRef(null);
+  const scrollHifiveRef = useRef(null);
+  const scrollSpeedoodleRef = useRef(null);
+
+  useEffect(() => {
+    const wheelHandler = (e) => {
+      e.preventDefault();
+      const { deltaY } = e;
+      const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+      const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+      if (deltaY > 0) {
+        // 스크롤 내릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          console.log('현재 1페이지, down');
+          window.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(() => 2);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          console.log('현재 2페이지, down');
+          window.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(() => 3);
+        } else {
+          // 현재 3페이지
+          console.log('현재 3페이지, down');
+          window.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(() => 3);
+        }
+      } else {
+        // 스크롤 올릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          console.log('현재 1페이지, up');
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(() => 1);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          console.log('현재 2페이지, up');
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(() => 1);
+        } else {
+          // 현재 3페이지
+          console.log('현재 3페이지, up');
+          window.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(() => 2);
+        }
+      }
+    };
+    const outerDivRefCurrent = outerDivRef.current;
+    outerDivRefCurrent.addEventListener('wheel', wheelHandler);
+    return () => {
+      outerDivRefCurrent.removeEventListener('wheel', wheelHandler);
+    };
+  }, []);
+
   return (
     <>
-      <HomeWrapper></HomeWrapper>
+      <HomeWrapper ref={outerDivRef}>
+        <HomeHeroContainer></HomeHeroContainer>
+        <Divider></Divider>
+        <HomeSentencyContainer></HomeSentencyContainer>
+        <Divider></Divider>
+        <HomeHiFiveContainer></HomeHiFiveContainer>
+        <Divider></Divider>
+        <HomeSpeedoodleContainer></HomeSpeedoodleContainer>
+      </HomeWrapper>
     </>
   );
 }
--- front/src/styles/HomeEmotion.js
+++ front/src/styles/HomeEmotion.js
@@ -3,10 +3,64 @@ import { colors } from '../styles/ColorPalette';
 
 export const HomeWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   width: 100%;
   height: 400vh;
+  background-color: ${colors.white};
+  box-sizing: border-box;
+  scoll: hidden;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const HomeHeroContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
   padding: 0 10%;
   background-color: ${colors.gameBlue100};
   box-sizing: border-box;
+`;
+
+export const HomeSentencyContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  padding: 0 10%;
+  background: linear-gradient(
+    to top right,
+    rgba(236, 61, 89, 0.5),
+    rgba(22, 86, 233, 0.5)
+  );
+  box-sizing: border-box;
+`;
+
+export const HomeHiFiveContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  padding: 0 10%;
+  background-color: ${colors.gameBlue200};
+  box-sizing: border-box;
+`;
+
+export const HomeSpeedoodleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  padding: 0 10%;
+  background-color: ${colors.gameBlue300};
+  box-sizing: border-box;
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: gray;
 `;

ee040fb85e2ac90a5d4080a14ecf6641f5797989
이연수 <noprob1325@naver.com>
2023-03-31 22:46:18 +0900

    [BE] Spring Boot - Speedoodle 방 퇴장 구현

--- back-springboot/src/main/java/com/swing/util/WebSocketConfig.java
+++ back-springboot/src/main/java/com/swing/util/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.swing.chat;
+package com.swing.util;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;

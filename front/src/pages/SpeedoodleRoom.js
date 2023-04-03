a49c78344d6eef56393fc78e9a59cb6e42702f8b
isks2 <isks2@naver.com>
2023-04-03 10:19:30 +0900

    [FE] speedoodle room userInfo 더미데이터 삭제 및 api data 연결

--- front/src/pages/SpeedoodleRoom.js
+++ front/src/pages/SpeedoodleRoom.js
@@ -20,92 +20,7 @@ function SpeedoodleRoom() {
   const [chatInput, setChatInput] = useState('');
   const [isGameStart, setIsGameStart] = useRecoilState(speedoodleGameState);
   const [chatData, setChatData] = useState([]);
-  const userImg =
-    'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRSM-bLdlw42S0tP6jHNppEhfDDU2nwKRL9UzKv7Mx6uOay9N4RsJLJmst9VIxAOckx';
-
-  const defaultUser = [
-    {
-      userId: 'red',
-      nickname: '행복한초코',
-      profileImgUrl: userImg,
-      leader: true,
-    },
-    {
-      userId: 'black',
-      nickname: '귀여운뽀삐',
-      profileImgUrl: userImg,
-      leader: false,
-    },
-    {
-      userId: 'mint',
-      nickname: '맹지니',
-      profileImgUrl: userImg,
-      leader: false,
-    },
-    {
-      userId: 'pink',
-      nickname: '인두목',
-      profileImgUrl: userImg,
-      leader: false,
-    },
-    {
-      userId: 'yellow',
-      nickname: '데이비드',
-      profileImgUrl: userImg,
-      leader: false,
-    },
-    {
-      userId: 'blue',
-      nickname: '쪼안나',
-      profileImgUrl: userImg,
-      leader: false,
-    },
-  ];
-
-  const gameStartUser = [
-    {
-      userId: 'red',
-      nickname: '행복한초코',
-      profileImgUrl: userImg,
-      leader: true,
-      time: '00:23:58',
-    },
-    {
-      userId: 'black',
-      nickname: '귀여운뽀삐',
-      profileImgUrl: userImg,
-      leader: false,
-      time: '00:27:28',
-    },
-    {
-      userId: 'mint',
-      nickname: '맹지니',
-      profileImgUrl: userImg,
-      leader: false,
-      time: '00:21:53',
-    },
-    {
-      userId: 'pink',
-      nickname: '인두목',
-      profileImgUrl: userImg,
-      leader: false,
-      time: '00:18:28',
-    },
-    {
-      userId: 'yellow',
-      nickname: '데이비드',
-      profileImgUrl: userImg,
-      leader: false,
-      time: '00:19:22',
-    },
-    {
-      userId: 'blue',
-      nickname: '쪼안나',
-      profileImgUrl: userImg,
-      leader: false,
-      time: '00:29:38',
-    },
-  ];
+  const [user, setUser] = useRecoilState(userState);
 
   // const sock = new SockJS(`${API_URL}/speedoodle/room`);
 
@@ -181,7 +96,7 @@ function SpeedoodleRoom() {
     // stomp.debug = null;
     const data = {
       roomId: room_id,
-      publisher: `user/${room_id}`,
+      publisher: user.nickname,
       message: chatInput,
     };
     if (stompRef.current?.connected) {
@@ -219,7 +134,7 @@ function SpeedoodleRoom() {
           {gameRoomInfo?.roomInfo ? (
             <>
               <SpeedoodleUser
-                data={isGameStart ? gameStartUser : defaultUser}
+                data={gameRoomInfo?.chatUserList}
               ></SpeedoodleUser>
 
               {isGameStart ? (

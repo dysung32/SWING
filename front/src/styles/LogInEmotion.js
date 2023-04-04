ca2fd146df7aeb2b57ada8c1f6db2809fbbd1d3c
isks2 <isks2@naver.com>
2023-04-04 14:39:05 +0900

    [FE] 로그인 페이지 퍼블리싱

--- front/src/styles/LogInEmotion.js
+++ front/src/styles/LogInEmotion.js
@@ -1,71 +1,77 @@
-import styled from "@emotion/styled";
-import logInExpImg from "../assets/login_expImg.png";
+import styled from '@emotion/styled';
+import LoginSide from '../assets/resize_login_side.gif';
 
 export const LogInWrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   width: 100vw;
   height: 100vh;
   padding: 0 10%;
   box-sizing: border-box;
-  background-color: #1656e9;
+  background: linear-gradient(
+    to top left,
+    rgba(0, 57, 188, 1),
+    rgba(253, 152, 152, 0.7)
+  );
 `;
 
 export const LogInContainer = styled.div`
-  display: grid;
-  grid-template-rows: repeat(7, 1fr);
-  grid-template-columns: repeat(12, 1fr);
-  column-gap: 2rem;
-  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 4rem;
+  width: 80%;
   height: 68vh;
   box-sizing: border-box;
   border-radius: 0.75rem;
-  background-color: #ffffff;
+  background-color: rgba(255, 255, 255, 0.7);
 `;
 
 export const ExpImg = styled.div`
-  grid-row-start: 2;
-  grid-row-end: 7;
-  grid-column-start: 2;
-  grid-column-end: 6;
-  background: center / contain no-repeat url(${logInExpImg});
+  width: 50%;
+  height: 100%;
+  border-radius: 0.75rem;
+  background: center / cover no-repeat url(${LoginSide});
 `;
+
+export const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 40%;
+  height: 100%;
+`;
+
 export const LogoImg = styled.div`
   display: flex;
   justify-content: center;
-  grid-row-start: 2;
-  grid-row-end: 5;
-  grid-column-start: 7;
-  grid-column-end: 12;
+  width: 100%;
+  height: 50%;
   box-sizing: border-box;
 `;
 
 export const LogInBtnContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  grid-row-start: 5;
-  grid-row-end: 7;
-  grid-column-start: 7;
-  grid-column-end: 12;
+  justify-content: end;
   box-sizing: border-box;
 `;
 
 export const LogInBtn = styled.div`
-  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  padding: 1.2rem 1rem 1.2rem 3.5rem;
-  border: ${(props) => (props.border ? `1px solid ${props.border}` : "none")};
+  padding: 1.2rem;
   border-radius: 0.5rem;
   box-sizing: border-box;
-  background-color: ${(props) => (props.color ? `${props.color}` : "#ffffff")};
+  background-color: #f7e600;
   text-align: center;
   font-size: 1.5rem;
   cursor: pointer;
 `;
 
 export const SocialLogoImg = styled.img`
-  position: absolute;
   top: 14px;
   left: 16px;
   width: 2.5rem;

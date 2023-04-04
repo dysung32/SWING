e3327850bded898a65235ebd353d4236ba341336
LEEYEONSOO <noprob1325@naver.com>
2023-04-04 17:34:35 +0900

    [BE] Spring Boot - Speedoodle 방 정보 불러오기 수정

--- back-springboot/src/main/java/com/swing/doodle/model/service/DoodleServiceImpl.java
+++ back-springboot/src/main/java/com/swing/doodle/model/service/DoodleServiceImpl.java
@@ -97,7 +97,7 @@ public class DoodleServiceImpl implements DoodleService {
 		List<ChatUserDto> chatUserDtoList = new ArrayList<>();
 		List<UserRoom> userRoomList = userRoomRepository.findAllByRoom_RoomId(roomId);
 		for (UserRoom userRoom : userRoomList) {
-			if (userRoom.getUser().getUserId() != userId)
+			if (!userId.equals(userRoom.getUser().getUserId()))
 				chatUserDtoList.add(ChatUserDto.toDto(userRoom.getUser()));
 		}
 		chatUserDtoList.add(ChatUserDto.toDto(userRepository.findByUserId(userId)));

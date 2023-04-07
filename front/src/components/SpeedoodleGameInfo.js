473c7e59b3107d5794b4c51c0575c63f136451b8
unknown <sanghwa1103@gmail.com>
2023-04-07 11:43:50 +0900

    image 등장

--- front/src/components/SpeedoodleGameInfo.js
+++ front/src/components/SpeedoodleGameInfo.js
@@ -53,7 +53,7 @@ function SpeedoodleGameInfo(props) {
     if (props.propMode === 1) {
       setLimits(() => 30);
     } else {
-      setLimits(() => 2);
+      setLimits(() => 20);
     }
   }, [props.propMode]);
 
@@ -80,7 +80,7 @@ function SpeedoodleGameInfo(props) {
     if (props.gameInfo.mode) {
       setLimits(() => 30);
     } else {
-      setLimits(() => 2);
+      setLimits(() => 20);
     }
   }, []);
 
